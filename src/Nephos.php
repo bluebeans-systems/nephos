17146ddf42b70f444cfe0a94d2a22aa48082883f
Ed Ryan Alburo <edryanalburo@coredev.ph>
2016-05-13 22:20:39 +0800

    added methods on posting of transaction and account

--- src/Nephos.php
+++ src/Nephos.php
@@ -2,8 +2,29 @@
 
 namespace BluebeansSystems\Nephos;
 
+use BluebeansSystems\Nephos\Commands\PostAccountCommand;
+use BluebeansSystems\Nephos\Commands\PostTransactionCommand;
 
 class Nephos {
 
+    /**
+     * Post Transaction
+     *
+     * @param $data
+     * @return mixed
+     */
+    public function postTransaction($data)
+    {
+        return $this->dispatchFrom(PostTransactionCommand::class, $data);
+    }
+
+    /**
+     * Post Account Transaction
+     * @param $data
+     */
+    public function postAccount($data)
+    {
+        $this->dispatchFrom(PostAccountCommand::class, $data);
+    }
 
 }
